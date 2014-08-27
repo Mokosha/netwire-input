44653d8497801ca8f77621e013046410a40c482e
Pavel Krajcevski <Krajcevski@gmail.com>
2014-08-26 23:59:50 -0400

    Add initial idea for MonadInput

---
+++ lib/FRP/Netwire/Input.hs
@@ -0,0 +1,51 @@
+module FRP.Netwire.Input (
+  Key, MouseButton,
+  MonadInput(..),
+) where
+
+--------------------------------------------------------------------------------
+import Control.Wire
+import Data.Monoid
+--------------------------------------------------------------------------------
+
+class Key a
+class MouseButton a
+
+class (Key k, MouseButton mb, Monad m) => MonadInput k mb m | m -> k, m -> mb where
+  keyIsPressed :: k -> m (Bool)
+  releaseKey :: k -> m ()
+  mbIsPressed :: mb -> m (Bool)
+  releaseButton :: mb -> m ()
+  cursor :: m (Float, Float)
+  scroll :: m (Double, Double)
+
+--------------------------------------------------------------------------------
+-- Wires
+
+keyPressed :: (Monoid e, MonadInput k mb m) => k -> Wire s e m a a
+keyPressed key = mkGen_ $ \x -> do
+  pressed <- keyIsPressed key
+  if pressed
+    then return (Right x)
+    else return (Left mempty)
+
+keyDebounced :: (Monoid e, MonadInput k mb m) => k -> Wire s e m a a
+keyDebounced key = mkGen_ $ \x -> do
+  pressed <- keyIsPressed key
+  if pressed
+    then releaseKey key >> return (Right x)
+    else return (Left mempty)
+
+mousePressed :: (Monoid e, MonadInput k mb m) => mb -> Wire s e m a a
+mousePressed mouse = mkGen_ $ \x -> do
+  pressed <- mbIsPressed mouse
+  if pressed
+    then return (Right x)
+    else return (Left mempty)
+
+mouseDebounced :: (Monoid e, MonadInput k mb m) => mb -> Wire s e m a a
+mouseDebounced mouse = mkGen_ $ \x -> do
+  pressed <- mbIsPressed mouse
+  if pressed
+    then releaseButton mouse >> return (Right x)
+    else return (Left mempty)
